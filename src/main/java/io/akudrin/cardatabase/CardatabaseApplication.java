76eb6e6ab1a61dc07996efdea348fe7d78dbc698
Andrei Kudrin <andreikudrin@gmail.com>
2019-03-29 10:33:48 +0000

    project structure with H2 database

---
+++ src/main/java/io/akudrin/cardatabase/CardatabaseApplication.java
@@ -0,0 +1,45 @@
+package io.akudrin.cardatabase;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.akudrin.cardatabase.domain.Car;
+import io.akudrin.cardatabase.domain.CarRepository;
+import io.akudrin.cardatabase.domain.Owner;
+import io.akudrin.cardatabase.domain.OwnerRepository;
+
+@SpringBootApplication
+public class CardatabaseApplication {
+	private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
+	// Inject repositories
+	@Autowired
+	private CarRepository repository;
+	@Autowired
+	private OwnerRepository orepository;
+	
+	public static void main(String[] args) {
+				SpringApplication.run(CardatabaseApplication.class, args);
+				logger.info("Hello Spring Boot");
+	}
+		@Bean
+		CommandLineRunner runner(){
+			return args -> {
+				
+				// Add owner objects and save these to db
+				Owner owner1 = new Owner("John" , "Johnson");
+				Owner owner2 = new Owner("Mary" , "Robinson");
+				orepository.save(owner1);
+				orepository.save(owner2);
+				
+				repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1));
+				repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2));
+				repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner2));
+			};
+			}
+	
+}
---
+++ src/main/java/io/akudrin/cardatabase/domain/OwnerRepository.java
@@ -0,0 +1,6 @@
+package io.akudrin.cardatabase.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+}
