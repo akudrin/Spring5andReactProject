f7d3494638887cab167196b0a414f4ccd2854ce5
Andrei Kudrin <andreikudrin@gmail.com>
2019-04-01 10:24:13 +0100

    added Jasypt (Java Simplified Encryption) Spring Boot for properties file

--- src/main/java/io/akudrin/cardatabase/CardatabaseApplication.java
+++ src/main/java/io/akudrin/cardatabase/CardatabaseApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 import io.akudrin.cardatabase.domain.Car;
 import io.akudrin.cardatabase.domain.CarRepository;
 import io.akudrin.cardatabase.domain.Owner;
